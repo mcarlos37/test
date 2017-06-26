92ef03a91a746b590c59946244277ea83e4bae0b
carlos.torres <carlos.torres@f14a119a-2264-413b-b695-c45686edb297>
2017-06-26 19:15:11 +0000

    se agregan propiedades a la configuracion de rest

--- BRANCHES/20170103-SMS-V1/conamerica_sms_service_backend/conamerica_sms_service_backend-web/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ BRANCHES/20170103-SMS-V1/conamerica_sms_service_backend/conamerica_sms_service_backend-web/src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -5,6 +5,15 @@
  */
 package org.netbeans.rest.application.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import javax.ws.rs.core.Application;
 
@@ -15,6 +24,30 @@ import javax.ws.rs.core.Application;
 @javax.ws.rs.ApplicationPath("rest")
 public class ApplicationConfig extends Application {
 
+    
+      private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    @Override
+    public Set<Object> getSingletons() {
+        Set<Object> set = new HashSet<>();
+        JacksonJsonProvider jacksonJsonProvider = new JacksonJaxbJsonProvider();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setDateFormat(DATE_FORMAT);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+        jacksonJsonProvider.setMapper(mapper);
+        set.add(jacksonJsonProvider);
+        return set;
+    }
+
+    @Override
+    public Map<String, Object> getProperties() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("jersey.config.disableMoxyJson.server", true);
+        return map;
+    }
+
+    
     @Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> resources = new java.util.HashSet<>();
