92b54576becc54c012354607a573dcd88b91baa9
carlos.torres <carlos.torres@f14a119a-2264-413b-b695-c45686edb297>
2017-06-29 00:06:49 +0000

    prueba del status

--- BRANCHES/20170103-SMS-V1/conamerica_sms_backend/conamerica_sms_backend_sql-ejb/src/main/java/com/grupoconamerica/backend/entity/facade/SmsMessageFacade.java
+++ BRANCHES/20170103-SMS-V1/conamerica_sms_backend/conamerica_sms_backend_sql-ejb/src/main/java/com/grupoconamerica/backend/entity/facade/SmsMessageFacade.java
@@ -78,7 +78,6 @@ public class SmsMessageFacade extends AbstractFacade<SmsMessage> implements SmsM
                 }
                 smsMessage.setSmsMessageProcessedStatus(SmsMessageProcessedStatus.SUCCESS);
                 em.merge(smsMessage);
-                em.persist(smsMessage);
                 smsMessages.add(smsMessage);
             }
 
